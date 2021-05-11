1cd0226b99ba9fe72872f17c0f5298c694d1f326
schweizercode <schweizercode@gmail.com>
2021-05-11 09:12:36 +0200

    style adjustments

--- src/components/CircleIcon.jsx
+++ src/components/CircleIcon.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Icon from "components/Icon";
-
 import "./CircleIcon.scss";
+
 
 const CircleIcon = ({ href, iconName }) => (
   <a
